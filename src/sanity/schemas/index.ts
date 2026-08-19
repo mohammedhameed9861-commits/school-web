cbd02e91d02f9a39f5eff351e63fbcff0cb55ea1
mohammedhameed9861-commits <mohammedhameed9861@gmail.com>
2026-08-19 15:57:10 +0300

    Add schemas index

---
+++ src/sanity/schemas/index.ts
@@ -0,0 +1,17 @@
+import { siteSettings } from "./siteSettings";
+import { heroContent } from "./heroContent";
+import { statsSection } from "./statsSection";
+import { advantage } from "./advantage";
+import { program } from "./program";
+import { achievement } from "./achievement";
+import { testimonial } from "./testimonial";
+
+export const schemaTypes = [
+  siteSettings,
+  heroContent,
+  statsSection,
+  advantage,
+  program,
+  achievement,
+  testimonial,
+];
